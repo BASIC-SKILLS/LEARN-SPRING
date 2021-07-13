aab33f8c6641237d34dbc8ecb975e9a6058ee790
HyeonJin-Jang <81272691+HyeonJin-Jang@users.noreply.github.com>
2021-07-13 23:14:48 +0900

    Delete SPRING directory

--- SPRING/src/main/java/com/koreait/first/board/BoardFavMapper.java
+++
@@ -1,7 +0,0 @@
-package com.koreait.first.board;
-
-public interface BoardFavMapper {
-    int insBoardFav(BoardFavEntity param);
-    int selBoardFav(BoardFavEntity param);
-    int delBoardFav(BoardFavEntity param);
-}
--- SPRING/src/main/java/com/koreait/first/board/BoardFavService.java
+++
@@ -1,5 +0,0 @@
-package com.koreait.first.board;
-
-public class BoardFavService {
-
-}
